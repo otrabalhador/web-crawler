7e76154d86db72ddca90a85a6c13e1a675b91f0a
Eryk Humberto Oliveira Alves <ealves@stone.com.br>
2023-05-07 16:19:35 -0300

    Implement web client

---
+++ internal/web_client/web_client.go
@@ -0,0 +1,36 @@
+package web_client
+
+import (
+	"io"
+	"net/http"
+	netUrl "net/url"
+	"web-crawler/internal"
+)
+
+type WebClient struct {
+}
+
+func NewWebClient() *WebClient {
+	return &WebClient{}
+}
+
+func (w WebClient) GetPageContent(url *netUrl.URL) (internal.Page, error) {
+	resp, err := http.Get(url.String())
+	if err != nil {
+		return internal.Page{}, err
+	}
+
+	defer func(Body io.ReadCloser) {
+		_ = Body.Close()
+	}(resp.Body)
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return internal.Page{}, err
+	}
+
+	return internal.Page{
+		Url:     internal.URL(*url),
+		Content: string(data),
+	}, nil
+}
